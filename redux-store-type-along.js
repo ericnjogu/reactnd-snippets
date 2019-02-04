9d176659870a295fc472608e45147c3767c85517
Eric Njogu <kunadawa@gmail.com>
2019-02-04 10:21:17 +0300

    feat: creating a function that acts like the redux store
    The createStoreFunction() returns an object whose properties are two functions - getState() and subscribe.
    
    Lots of ES6 code in use

---
+++ redux-store-type-along.js
@@ -0,0 +1,27 @@
+function createStore() {
+    // variables whose value can change are declared by the let keyword in ES6
+    let state
+    let listeners = []
+
+    // functions are declared by the const keyword since their value will not be modified
+    const getState = () => state
+
+    const subscribe = (listener) => {
+        listeners.push(listener)
+        return () => {
+            listeners = listeners.filter((l) => l !== listener)
+        }
+    }
+    // returns an object whose two props are functions
+    return {getState, subscribe}
+}
+
+// two subscribers each returning a ref to an unsubscriber function
+const store = createStore()
+unsub1 = store.subscribe(()=> {
+    console.log(`Listener 1: The new state is ${store.getState}`)
+})
+
+unsub2 = store.subscribe(()=> {
+    console.log(`Listener 2: The new state is ${store.getState}`)
+})
